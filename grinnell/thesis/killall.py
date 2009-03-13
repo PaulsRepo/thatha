e4b391ccca2530764eb2dd70b102ceaf11b0cf32
theocharis.athanasakis <theocharis.athanasakis@c2fd7406-0774-11de-b753-7319d013a8fa>
2009-03-13 06:02:30 +0000

    utility to run on mathlan

--- grinnell/thesis/killall.py
+++ grinnell/thesis/killall.py
@@ -7,4 +7,4 @@ import os
 for i in alphabet:
         m = machines.pop()
         print "running %s on %s" %(i,m)
-        os.system('ssh %s "killall python"' % m)
+        os.system('ssh %s "killall python & ps aux"' % m)
