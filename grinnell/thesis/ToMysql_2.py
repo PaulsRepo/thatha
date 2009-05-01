10b0eafecdd2c890e251360d2b91027898085b57
thatha7777 <thatha7777@c2fd7406-0774-11de-b753-7319d013a8fa>
2009-05-01 02:18:56 +0000

    fb selection

--- grinnell/thesis/ToMysql_2.py
+++ grinnell/thesis/ToMysql_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 import os
 
-print "ALTER TABLE ud ADD blogsearch int;"
-
-
 count = 0
 for file in os.listdir('data/'):
 	if file.startswith('goog-'):
@@ -17,4 +14,3 @@ for file in os.listdir('data/'):
 				y = line.split('\t')
 				print "UPDATE ud SET blogsearch = %d WHERE lemma = '%s';" % (int(y[1]), y[0].replace("\\", "\\\\").replace("'", "\\'").replace("\"", "\\\""))
 				count += 1
-print footer
